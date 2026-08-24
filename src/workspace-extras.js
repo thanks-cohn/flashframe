612306db0c10ce371eb17ae97aa6fed98540ed95
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-23 21:30:19 -0500

    Restore workspace extras before mascot work

--- src/workspace-extras.js
+++ src/workspace-extras.js
@@ -1,1 +1,59 @@
-placeholder
+const href = new URL("./web-drop.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === href)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.append(link);
+}
+
+const polishHref = new URL("./media-ux-polish.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === polishHref)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = polishHref;
+  document.head.append(link);
+}
+
+const themeHref = new URL("./theme-customization.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === themeHref)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = themeHref;
+  document.head.append(link);
+}
+
+const mediaDockHref = new URL("./media-dock-layout.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === mediaDockHref)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = mediaDockHref;
+  document.head.append(link);
+}
+
+const donationHref = new URL("./donation-card.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === donationHref)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = donationHref;
+  document.head.append(link);
+}
+
+const frameChuteFinalHref = new URL("./framechute-final-polish.css", import.meta.url).href;
+if (![...document.styleSheets].some((sheet) => sheet.href === frameChuteFinalHref)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = frameChuteFinalHref;
+  document.head.append(link);
+}
+
+await import("./web-drop.js");
+await import("./drop-local-sources.js");
+await import("./remote-video.js");
+await import("./layer-menu.js");
+await import("./appearance.js");
+await import("./media-ux-polish.js");
+await import("./theme-customization.js");
+await import("./media-dock-layout.js");
+await import("./media-header-theme.js");
+await import("./donation-card.js");
+await import("./framechute-final-polish.js");
